7dae392febbf6c5322974b9b1ad56fbd7311202f
joaoftz <joaomarcosftz04@gmail.com>
2024-10-29 18:13:55 -0300

    atts no arquivos da liquid

--- Springs/liquidsa/src/main/java/com/example/liquidsa/entities/ComentarioEntity.java
+++ Springs/liquidsa/src/main/java/com/example/liquidsa/entities/ComentarioEntity.java
@@ -10,7 +10,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
-@Entity(name = "conexao")
+@Entity(name = "comentario")
 
 public class ComentarioEntity {
     @Id
